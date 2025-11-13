d6478b8ce6508c2237f7a9cb7d38ab7e20212741
Richard567-max <2906104589@qq.com>
2025-11-13 21:29:50 +0800

    后端框架

---
+++ qingyu_app_backend/src/main/java/com/qingyu/eye/qingyu_app_backend/QingyuAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qingyu.eye.qingyu_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingyuAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingyuAppBackendApplication.class, args);
+    }
+
+}
